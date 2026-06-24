e5975937c483ae97c50158d05d7adab911879eec
UFifty50sh <ufifty50.github@gmail.com>
2026-06-24 17:04:41 +0100

    .

--- showErrors.sh
+++ showErrors.sh
@@ -1,13 +1,15 @@
 echo "1. Build datapack into example world"
-bun i
-sand build
+echo ">> bun i && sand build"
+bun i && sand build
 
 echo
 
 echo "2. Install a native package for one error"
+echo ">> sand install native block"
 sand install native block
 
 echo
 
 echo "3. Install a vanilla package for another error"
+echo ">> sand install vanilla player_motion"
 sand install vanilla player_motion
